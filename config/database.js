a034674fb5c800524f29964dac7818748ccef57a
julysNICK <julystube@gmail.com>
2022-09-12 11:27:14 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', ''),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', ''),
+    },
+
+    pool: {
+      min: env.int('DATABASE_POOL_MIN', 0),
+      max: env.int('DATABASE_POOL_MAX', 5),
+    },
+  },
+});
